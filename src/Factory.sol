c8b39ef91b8fe4158237d2ed9ecea5c99648f3a7
Timur Badretdinov <destinerx@gmail.com>
2022-08-28 01:15:29 +0400

    Initialize Factory contract

---
+++ src/Factory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./Auction.sol";
+
+contract Factory {
+    function createAuction(
+        address tokenBase,
+        address tokenQuote,
+        uint256 amountBase,
+        uint256 initialPrice,
+        uint256 halvingPeriod,
+        uint256 swapPeriod
+    )
+        external
+        returns (address auction)
+    {
+        require(tokenBase != tokenQuote);
+        auction = address(new Auction(tokenBase, tokenQuote, amountBase, initialPrice, halvingPeriod, swapPeriod));
+    }
+}
