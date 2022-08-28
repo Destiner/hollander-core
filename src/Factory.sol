1882ca08672e2ac27da5b1ee65a13d18a1098ada
Timur Badretdinov <destinerx@gmail.com>
2022-08-28 15:59:37 +0400

    Add NewAuction event to Factory

--- src/Factory.sol
+++ src/Factory.sol
@@ -4,6 +4,16 @@ pragma solidity 0.8.13;
 import "./Auction.sol";
 
 contract Factory {
+    event NewAuction(
+        address indexed owner,
+        address indexed tokenBase,
+        address indexed tokenQuote,
+        uint256 amountBase,
+        uint256 initialPrice,
+        uint256 halvingPeriod,
+        uint256 swapPeriod
+);
+
     function createAuction(
         address tokenBase,
         address tokenQuote,
@@ -18,5 +28,6 @@ contract Factory {
         require(tokenBase != tokenQuote);
         auction =
             address(new Auction(msg.sender, tokenBase, tokenQuote, amountBase, initialPrice, halvingPeriod, swapPeriod));
+        emit NewAuction(msg.sender, tokenBase, tokenQuote, amountBase, initialPrice, halvingPeriod, swapPeriod);
     }
 }
